1f62a772b9b61f6f665bfae85eee7cf89c96403b
GordonAnsell <moving@ga1964.com>
2022-03-12 06:38:49 +0000

    Update complexImage.js

--- src/html/complexImage.js
+++ src/html/complexImage.js
@@ -164,10 +164,10 @@ class ComplexImage
         sourceGenNoScript.setAttrib('type', files[0].mime);
 
         // Lazyload check.
+        syslog.warning(`${files[0].url}: ${files.length}`)
         if (this.lazyload && 1 === files.length) {
             syslog.warning(`Really small images and lazyloading is a problem: ${files[0].url}`);
         }
-
 
         // Loop for each file.
         for (let item of files.filter(f => f.width !== this.config.placeholderWidth)) {
