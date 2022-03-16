3c753d5229005c6e7c193059898a829bdc4ca3bd
GordonAnsell <moving@ga1964.com>
2022-03-16 09:28:43 +0000

    Update typeBase.js

--- src/schema/types/typeBase.js
+++ src/schema/types/typeBase.js
@@ -10,7 +10,7 @@
 
 const path = require('path');
 const url = require('url');
-const YamlFile = require("../yamlFile");
+const YamlFile = require("../../yamlFile");
 const GAError = require("../utils/gaError");
 const syslog = require("../logger/syslog");
 
